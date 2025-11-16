3e1f8b011955f388545593b66fc940f3af72d281
Alex Gurganus <gurganusalex@gmail.com>
2025-11-16 12:52:20 -0600

    format with csharpier

--- src/MainMenu/Popup/BackupHelp/BackupHelp.cs
+++ src/MainMenu/Popup/BackupHelp/BackupHelp.cs
@@ -4,18 +4,18 @@ using Godot;
 
 public partial class BackupHelp : SceneData
 {
-	[Export]
-	public SceneData popup;
+    [Export]
+    public SceneData popup;
 
-	// MATCH: DefineSprite_2407-frame_2-PlaceObject2_2382_1-CLIPACTIONRECORD on(release).as
-	public override void _Input(InputEvent @event)
-	{
-		this.GetViewport().SetInputAsHandled();
-		if (Input.IsActionJustReleased("leftclick"))
-		{
-			popup.Unload();
-		}
-	}
+    // MATCH: DefineSprite_2407-frame_2-PlaceObject2_2382_1-CLIPACTIONRECORD on(release).as
+    public override void _Input(InputEvent @event)
+    {
+        this.GetViewport().SetInputAsHandled();
+        if (Input.IsActionJustReleased("leftclick"))
+        {
+            popup.Unload();
+        }
+    }
 
-	public override void _Process(double delta) {}
+    public override void _Process(double delta) { }
 }
