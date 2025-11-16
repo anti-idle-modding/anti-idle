25a9a5f57c8784c1398cd8d219360453e25c511b
Alex Gurganus <gurganusalex@gmail.com>
2025-11-16 12:52:20 -0600

    format with csharpier

--- src/MainMenu/CreditsButton.cs
+++ src/MainMenu/CreditsButton.cs
@@ -5,11 +5,11 @@ using Godot;
 // MATCH: frame_11-PlaceObject2_1602_44-CLIPACTIONRECORD on(release).as
 public partial class CreditsButton : Button
 {
-	[Export]
-	public SceneManager popup;
+    [Export]
+    public SceneManager popup;
 
-	public override void _Pressed()
-	{
-		popup.Show("Credits");
-	}
+    public override void _Pressed()
+    {
+        popup.Show("Credits");
+    }
 }
